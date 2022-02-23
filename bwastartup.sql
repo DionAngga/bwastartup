695c3a6a962e47cda864ad35121f8a2463112f35
unknown <deonisius97@gmail.com>
2022-02-23 10:46:25 +0700

    create campaign

--- bwastartup.sql
+++ bwastartup.sql
@@ -1,1 +1,174 @@
-SELECT * FROM bwastartup.user;
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 22, 2022 at 11:00 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bwastartup`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `campaigns`
+--
+
+CREATE TABLE `campaigns` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `short_description` varchar(255) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `perks` text DEFAULT NULL,
+  `backer_count` int(11) DEFAULT NULL,
+  `goal_amount` int(11) DEFAULT NULL,
+  `current_amount` int(11) DEFAULT NULL,
+  `slug` varchar(255) DEFAULT NULL,
+  `craeted_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `campaign_images`
+--
+
+CREATE TABLE `campaign_images` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `campaign_id` int(11) DEFAULT NULL,
+  `file_name` varchar(255) DEFAULT NULL,
+  `is_primary` tinyint(4) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `campaign_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `code` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `transactions`
+--
+
+INSERT INTO `transactions` (`id`, `campaign_id`, `user_id`, `amount`, `status`, `code`, `created_at`, `deleted_at`) VALUES
+(1, 1, 1, 323, NULL, NULL, '2022-02-22 10:07:38', '2022-02-22 10:07:38');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `occupation` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password_hash` varchar(255) DEFAULT NULL,
+  `avatar_file_name` varchar(255) DEFAULT NULL,
+  `role` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `occupation`, `email`, `password_hash`, `avatar_file_name`, `role`, `created_at`, `updated_at`) VALUES
+(1, 'ichigo', 'programmer', 'ichigo@gmail.com', 'ichigo123', 'avatar.jpg', 'user', '2022-02-22 11:12:42', NULL),
+(2, 'kurosaki', 'konsultan ', 'kurosaki@gmail.com', 'kurosaki123', 'avatar2.jpg', 'user', '2022-02-22 11:13:07', NULL),
+(3, 'test data', '', '', '', '', '', '2022-02-22 14:08:47', '2022-02-22 14:08:47'),
+(4, 'tes simpan dari service', 'ninja', 'test@gmail.com', '$2a$04$7.qUTzGsN2h1gjfhPxQkauw.2sxbD94lWwACSSSG.HgSs4PU3joHu', '', 'user', '2022-02-22 14:47:31', '2022-02-22 14:47:31'),
+(5, 'Nama dari Post', 'UI designer', 'emai@gmail.com', '$2a$04$MWp0g9NLM16b7KspN2cgEefOkjmO99wsZziCdTqAu4.IW1/IL.4qO', '', 'user', '2022-02-22 15:35:25', '2022-02-22 15:35:25'),
+(6, 'Nama', 'presiden', 'emai@gmail.com', '$2a$04$wxrkLBIWNHMu80rA1CAsueTWhpoNwOHWVOSF2.kENwtyiAJTYTfZm', '', 'user', '2022-02-22 16:01:32', '2022-02-22 16:01:32'),
+(7, 'data', 'wakil', 'emai@gmail.com', '$2a$04$9n5KLZhDXf2/dv1IdZLeIumqNEcX2v1RR56uhY4NuMNYPfof2.yWO', '', 'user', '2022-02-22 16:05:09', '2022-02-22 16:05:09');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `campaigns`
+--
+ALTER TABLE `campaigns`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `campaign_images`
+--
+ALTER TABLE `campaign_images`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `campaigns`
+--
+ALTER TABLE `campaigns`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `campaign_images`
+--
+ALTER TABLE `campaign_images`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
