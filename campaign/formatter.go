8f20089e72e07857d6cc364b4af120b5a1323c44
unknown <deonisius97@gmail.com>
2022-06-16 23:43:40 +0700

    get campaigns endpoint formatter

--- campaign/formatter.go
+++ campaign/formatter.go
@@ -1,21 +1,36 @@
 package campaign
 
-type CampsFormatter struct {
-	UserID           int    `json:"user_id"`
-	ID               int    `json:"id"`
-	Name             string `json:"name"`
-	Description      string `json:"description"`
+type CampaignFormatter struct {
+	UserID int    `json:"user_id"`
+	ID     int    `json:"id"`
+	Name   string `json:"name"`
+	//Description 	string `json:"description"`
 	ShortDescription string `json:"short_description"`
+	ImageURL         string `json:"image_url"`
+	GoalAmount       int    `json:"goal_amount"`
+	CurrentAmount    int    `json:"current_amount"`
 }
 
-func FormatCamps(campaign Campaign) CampsFormatter {
-	formatter := CampsFormatter{
-		UserID:           campaign.UserID,
-		ID:               campaign.ID,
-		Name:             campaign.Name,
-		Description:      campaign.Description,
-		ShortDescription: campaign.ShortDescription,
-	}
+func FormatCampaign(campaign Campaign) CampaignFormatter {
+	CampaignFormatter := CampaignFormatter{}
+	CampaignFormatter.UserID = campaign.UserID
+	CampaignFormatter.ID = campaign.ID
+	CampaignFormatter.Name = campaign.Name
+	CampaignFormatter.ShortDescription = campaign.ShortDescription
+	CampaignFormatter.GoalAmount = campaign.GoalAmount
+	CampaignFormatter.CurrentAmount = campaign.CurrentAmount
+	CampaignFormatter.ImageURL = ""
 
-	return formatter
+	if len(campaign.CampaignImages) > 0 {
+		CampaignFormatter.ImageURL = campaign.CampaignImages[0].FileName
+	}
+	return CampaignFormatter
+}
+
+func FormatCampaigns(campaigns []Campaign) []CampaignFormatter {
+	campaignFormatter := []CampaignFormatter{}
+	for _, campaign := range campaigns {
+		campaignFormatter = append(campaignFormatter, FormatCampaign(campaign))
+	}
+	return campaignFormatter
 }
